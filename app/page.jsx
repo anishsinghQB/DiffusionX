40e701beaeb24012f7933ae4eb8973dc2888541c
anishsinghQB <anish.singh@quadbtech.com>
2025-09-25 17:23:48 +0530

    fix frontend UI issue

--- app/page.jsx
+++
@@ -1,9 +0,0 @@
-import ImageGenerator from "../components/image-generator";
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-indigo-600 to-purple-600">
-      <ImageGenerator />
-    </main>
-  );
-}
